7dc409b90c3d1c976f3c14d1398adc489abb3c00
akakss225 <akakss225@naver.com>
2024-03-27 19:43:06 +0900

    MemberEntity 수정2차

--- src/entities/member/MemberEntity.tsx
+++ src/entities/member/MemberEntity.tsx
@@ -1,18 +1,19 @@
 import { Member } from './model/member-info';
-import style from './ui/member.module.scss'
+import styles from './ui/member.module.scss'
 
 interface MemberEntityProps {
     param:Member
 }
+
 export function MemberCard({ param }: MemberEntityProps){
     return(
-        <div className={style.container}>
-            <ul>
-                <li>{param.name}</li>
-                <li>{param.gender}</li>
-                <li>{param.age}</li>
-                <li>{param.smoking_yn}</li>
-                <li>{param.religion}</li>
+        <div className={styles.container}>
+            <ul className={styles.memberline}>
+                <li className={styles.memberinfo}>{param.name}</li>
+                <li className={styles.memberinfo}>{param.gender}</li>
+                <li className={styles.memberinfo}>{param.age}</li>
+                <li className={styles.memberinfo}>{param.smoking_yn}</li>
+                <li className={styles.memberinfo}>{param.religion}</li>
             </ul>
         </div>
     );
