f8204530dbf0e48863af2179b6413559d976ffd9
akakss225 <akakss225@naver.com>
2024-04-15 20:49:34 +0900

    2024-04-15 작업 전 push

--- src/shared/shared.tsx
+++ src/shared/shared.tsx
@@ -1,6 +1,7 @@
 export { Match } from "./component/Match/Match";
 export { ConditionBox } from "./component/ConditionBox/ConditionBox";
 export { ItemListPanel } from "./component/ItemListPanel/ItemListPanel";
+export { Member } from "./type/Entity/Member"
 export { MemberFormDTO } from "./type/DTO/MemberFormDTO";
 export { AddressFormDTO } from "./type/DTO/AddressFormDTO";
 export { ConditionListModel, Filtering } from "./type/DTO/ConditionListModel";
