57b7d4d4fedce5e8f7d0b369189c17c0603dec99
Lee Han Seong <jkhk7788@gmail.com>
2024-04-16 21:50:17 +0900

    Update AddressFormDTO.ts

--- src/shared/type/DTO/AddressFormDTO.ts
+++ src/shared/type/DTO/AddressFormDTO.ts
@@ -1,11 +1,11 @@
-import { Location } from "../../shared";
+import { GeoPoint } from "../../shared";
 
 export class AddressFormDTO {
 	zipcode: string | null = null;
 	sido: string | null = null;
 	sigungu: string | null = null;
 	eupmyundong: string | null = null;
-	location: Location = new Location();
+	location: GeoPoint = new GeoPoint();
 
 	constructor(data: Partial<AddressFormDTO> = {}) {
 		Object.assign(this, data);
