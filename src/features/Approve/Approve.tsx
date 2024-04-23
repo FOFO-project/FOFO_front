a3d14aa25d9c10185fd10caf096315020f3d964a
Lee Han Seong <jkhk7788@gmail.com>
2024-04-23 22:57:53 +0900

    입금확인 & 거절 & 확정 버튼 기능 연결
    입금확인 & 거절 & 확정 버튼 기능 연결

--- src/features/Approve/Approve.tsx
+++ src/features/Approve/Approve.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import { useNavigate } from "react-router-dom";
 import style from "../features.module.scss";
+import { getResult } from "./api/getResult";
 
 interface ApproveProps {
 	selected: number[];
@@ -10,9 +11,8 @@ export function Approve({ selected }: ApproveProps) {
 	const navigate = useNavigate();
 	async function approveSelected() {
 		try {
-			//const result = await getResult(conditionData);
-			//setMembers(result);
-			console.log(selected);
+			const result = await getResult(selected);
+			alert(`확정완료 완료. (실패 : ${result.length}건)`);
 			navigate("/MemberManage");
 		} catch (err) {
 			alert("확정에 실패하였습니다. 관리자에게 문의 부탁드립니다.");
--- src/features/ConfirmDeposit/ConfirmDeposit.tsx
+++ src/features/ConfirmDeposit/ConfirmDeposit.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import classNames from "classnames";
 import style from "../features.module.scss";
+import { getResult } from "./api/getResult";
 
 interface ConfirmDepositProps {
 	selected: number[];
@@ -10,9 +11,8 @@ export function ConfirmDeposit({ selected }: ConfirmDepositProps) {
 	const navigate = useNavigate();
 	async function confirmSelected() {
 		try {
-			//const result = await getResult(conditionData);
-			//setMembers(result);
-			console.log(selected);
+			const result = await getResult(selected);
+			alert(`입금확인 완료. (실패 : ${result.length}건)`);
 			navigate("/ApprovalManage");
 		} catch (err) {
 			alert("입금확인에 실패하였습니다. 관리자에게 문의 부탁드립니다.");
