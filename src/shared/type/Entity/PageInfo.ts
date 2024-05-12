2841a4f76a3eb2908bd02715a7ab6679ab42d45b
Lee Han Seong <jkhk7788@gmail.com>
2024-05-12 16:43:38 +0900

    Update PageInfo.ts

--- src/shared/type/Entity/PageInfo.ts
+++ src/shared/type/Entity/PageInfo.ts
@@ -1,6 +1,6 @@
 export class PageInfo {
 	page: number = 0;
-	size: number = 30;
+	size: number = 2;
 	totalElements: number = 0;
 	totalPages: number = 0;
 	constructor(data: Partial<PageInfo> = {}) {
