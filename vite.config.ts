ce4cd5603d4709385c7d38616051efde853bdd07
Lee Han Seong <jkhk7788@gmail.com>
2024-05-12 16:02:47 +0900

    init github action workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 
@@ -15,4 +14,5 @@ export default defineConfig({
 		},
 		cors: true,
 	},
+	base: "fofo-frontend",
 });
